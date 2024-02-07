390b52d208a87927813796ed519de3a7d05d472d
lenovo <lenovo@DESKTOP-BR3R57V>
2024-02-07 23:38:10 +0530

    New Changes Should Be Done

--- SeleniumPractice/src/main/java/automationPractice/AutomationPractice.java
+++ SeleniumPractice/src/main/java/automationPractice/AutomationPractice.java
@@ -1,11 +1,18 @@
 package automationPractice;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class AutomationPractice {
 
 	static WebDriver driver ;
 
 	public static void main(String[] args) {
+		
+		driver = new EdgeDriver();
+		driver.close();
+		
+		
+		
 	}
 
 }
