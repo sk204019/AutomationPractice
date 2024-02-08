7672b379dd20cbff2663fcf4a096ff95f875c46c
lenovo <lenovo@DESKTOP-BR3R57V>
2024-02-08 12:14:54 +0530

    Commit

--- SeleniumPractice/src/main/java/automationPractice/WebDriverMethods.java
+++ SeleniumPractice/src/main/java/automationPractice/WebDriverMethods.java
@@ -1,7 +1,20 @@
 package automationPractice;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+/*
+get()
+getCurrentUrl()
+getTitle()
+getPageSource()
+getText()
+getTagName()
+getCssValue()
+getAttribute(String Name)
+getSize()
+close()
+quit() */
 
 public class WebDriverMethods {
 
@@ -14,7 +27,9 @@ public class WebDriverMethods {
 
 		//1st String Url
 		driver.get("https://opensource-demo.orangehrmlive.com/");
-
+		
+		/*
+		 
 		//2nd CurrentUrl
 		driver.getCurrentUrl();
 		System.out.println(driver.getCurrentUrl());
@@ -22,8 +37,48 @@ public class WebDriverMethods {
 		//3rd
 		driver.getPageSource();
 		System.out.println(driver.getPageSource());
-		//driver.
+
+		//4th 
+		driver.getTitle();
+		System.out.println(driver.getTitle());
+
+		//7th  manage
+
+		driver.manage().deleteAllCookies();
+
+		//8th navigate
+
+		driver.navigate().back();
+
+		//12th  & 13th
+
+		driver.quit();
+		driver.close();
+
+
+		//11th switchTo
+
+		driver.switchTo().hashCode();
+
+		//9th
+
+		driver.findElement(By.name("name=\"username\""));
+
+
+		//10th
+		driver.findElements(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/div/div"));
+
+*/
+		//5th
+		driver.getWindowHandle();
+		System.out.println(driver.getWindowHandle());
+
+		//6th
+		driver.getWindowHandles();
+
+
 
 	}
 
 }
+
