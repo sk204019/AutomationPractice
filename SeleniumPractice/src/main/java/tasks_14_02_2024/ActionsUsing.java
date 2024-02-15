e58b3e5e7022d92f17e6cb64715eb3771c6f405d
lenovo <lenovo@DESKTOP-BR3R57V>
2024-02-15 13:03:46 +0530

    Tasks_14_02_2024

---
+++ SeleniumPractice/src/main/java/tasks_14_02_2024/ActionsUsing.java
@@ -0,0 +1,51 @@
+package tasks_14_02_2024;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+/*
+mousehover
+RightClick
+Drag And Drop
+Double Click 
+ */
+public class ActionsUsing {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+		/*
+		driver.get("https://www.amazon.in/");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		//mousehover
+		WebElement mouseOperationPerform = driver.findElement(By.xpath("//span[@class='nav-line-2 ']//span[@style='visibility: visible;']"));
+
+		Actions action = new Actions(driver);
+		action.moveToElement(mouseOperationPerform).build().perform();
+		action.click();
+		//action.doubleClick();
+		if (driver.getCurrentUrl().contains("https://www.amazon.in/ap/signin")) {
+			System.out.println("succesfully handeld mose hover operation of sig in page");
+		} else {
+			System.out.println("user failed to handle through mouse operation of sign in page");
+		}
+		driver.quit();
+		 */
+		driver.get("https://testautomationpractice.blogspot.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		WebElement dragToMeMyTarget =	driver.findElement(By.id("draggable"));
+		WebElement dropHere = driver.findElement(By.id("droppable"));
+		Actions action = new Actions(driver);
+		action.dragAndDrop(dragToMeMyTarget, dropHere);
+System.out.println("111");
+	}
+
+}
--- SeleniumPractice/src/main/java/tasks_14_02_2024/Amazon_DynamicDropDown.java
+++ SeleniumPractice/src/main/java/tasks_14_02_2024/Amazon_DynamicDropDown.java
@@ -1,20 +1,43 @@
 package tasks_14_02_2024;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Amazon_DynamicDropDown {
 
 	public static void main(String[] args) {
-		
+
 		WebDriver driver = new ChromeDriver();
-		
+
 		driver.get("https://www.amazon.in/");
-		
-		driver.findElement(By.xpath("//form[@name='site-search']")).sendKeys("apple");
-		
-		driver.findElements(By.tagName(""));
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys("apple");
+
+		List< WebElement> listOfSuggestion = driver.findElements(By.xpath("//div[@class='left-pane-results-container']//div[@class='s-suggestion-container']//span"));
+		System.out.println(listOfSuggestion.size());
+
+
+		//only charger
+		//	List< WebElement> suggestionDropdowns = driver.findElements(By.xpath("//div[@class='left-pane-results-container']//div[@role='button']//span[text()=' charger']"));		
+		//	System.out.println(suggestionDropdowns.size());
+
+		for (WebElement options : listOfSuggestion) {
+
+			String suggestionsPrint = options.getText();
+			System.out.println(options.getText());
+
+			if (suggestionsPrint.contains("charger")) {
+				options.click();
+
+			}
+		}
+
 	}
 
 }
